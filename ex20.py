55852d7cfe3158804ac9a7ab86e7ab707703cc6c
didivava <divaulia@gmail.com>
2020-11-18 07:51:21 +0700

    Delete ex20.py

--- ex20.py
+++
@@ -1,7 +0,0 @@
-p = float(input("Enter preassure in pascal = "))
-v = float(input("Enter volume in liter = "))
-t = float(input("Enter temperature in celcius = "))
-
-n = (p * v) / (8.314 * (273.15 + t))
-
-print("%.2f moles of gas in the tank" %n )
