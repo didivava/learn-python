abced2c6a6b2bc0a8e0fa5dbb5683742d07f8023
didivava <divaulia@gmail.com>
2020-11-18 07:51:31 +0700

    Delete ex23.py

--- ex23.py
+++
@@ -1,8 +0,0 @@
-import math
-
-n = int(input("Enter numbers of side = "))
-s = int(input("Enter length of side = "))
-
-area = (n * s * s) / (4 * math.tan(math.pi / n))
-
-print("The area is %.2f" % area)
