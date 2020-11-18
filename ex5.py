e0237241d53e7ffcd770510a43b62d56745f9025
didivava <divaulia@gmail.com>
2020-11-18 07:52:29 +0700

    Delete ex5.py

--- ex5.py
+++
@@ -1,6 +0,0 @@
-small = input("Enter number of small bottle (<= 1 litre) = ")
-large = input("Enter number of large bottle (> 1 litre) = ")
-
-refund = (float(small) * 0.1) + (float(large) * 0.25)
-
-print("Refund = $ %.2f" %refund)
